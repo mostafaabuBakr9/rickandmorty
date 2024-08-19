d095358ca3132b05cd3b94309f94902d95fb7d46
AbuBakr14 <mostafaabubakr9@gmail.com>
2024-08-19 05:34:18 +0300

    code refactor for custom character item file

--- lib/Presentation_layer/Widgets/custom_character_item.dart
+++ lib/Presentation_layer/Widgets/custom_character_item.dart
@@ -20,6 +20,36 @@ class CharacterItem extends StatelessWidget {
           //  color: MyColors.grey
         ),
         child: GridTile(
+            footer: GridTileBar(
+              backgroundColor: Colors.black38,
+              title: Text(
+                character.name,
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            header: GridTileBar(
+                trailing: Text(
+                  character.gender,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                backgroundColor: Colors.black26,
+                leading: CharacterStatus(character: character),
+                title: Text(
+                  character.status,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
             child: Container(
               child: character.image.isNotEmpty
                   ? FadeInImage.assetNetwork(
@@ -29,38 +59,6 @@ class CharacterItem extends StatelessWidget {
                       fit: BoxFit.cover,
                     )
                   : Image.asset('Assets/Images/no-image-available.jpg'),
-            ),
-            footer: GridTileBar(
-              backgroundColor: Colors.black38,
-              title: Text(
-                '${character.name}',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            header: Container(
-              child: GridTileBar(
-                  trailing: Text(
-                    '${character.gender}',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  backgroundColor: Colors.black26,
-                  leading: CharacterStatus(character: character),
-                  title: Text(
-                    '${character.status}',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  )),
             )));
   }
 }
