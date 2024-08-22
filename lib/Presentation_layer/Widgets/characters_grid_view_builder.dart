e342b6081891bbff4b15766001424eae7e578b09
AbuBakr14 <mostafaabubakr9@gmail.com>
2024-08-22 04:01:55 +0300

    and controller to CharactersGridViewBuilder widget

--- lib/Presentation_layer/Widgets/characters_grid_view_builder.dart
+++ lib/Presentation_layer/Widgets/characters_grid_view_builder.dart
@@ -1,13 +1,21 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+
 import 'package:rickandmorty/Presentation_layer/Widgets/custom_character_item.dart';
 
 class CharactersGridViewBuilder extends StatelessWidget {
   final List characters;
-  const CharactersGridViewBuilder({super.key, required this.characters});
+  ScrollController? controller;
+  CharactersGridViewBuilder({
+    Key? key,
+    required this.characters,
+    this.controller,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GridView.builder(
+      controller: controller,
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 0.698,
